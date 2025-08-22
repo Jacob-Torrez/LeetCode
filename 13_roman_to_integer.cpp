b4cbf1b26cf702cd42d26a298bc54a39af5140b7
Jacob Torrez <72826559+Jacob-Torrez@users.noreply.github.com>
2025-08-22 10:53:05 -0700

    Update 13_roman_to_integer.cpp - LeetCode Problem 13

--- 13_roman_to_integer.cpp
+++ 13_roman_to_integer.cpp
@@ -3,20 +3,26 @@ public:
     int romanToInt(string s) {
         unordered_map<char, int> symbols = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
 
-        int last;
+        int last = -1;
         int current;
-        int sub = 0;
         int total = 0;
 
-        for (int i = 0; i < s.length() - 1; i++){
-            last = symbols[s[i]];
-            current = symbols[s[i + 1]];
-            
-            if (sub > 0) {total += last - sub; sub = 0;}
-            else if (last >= current){total += last;}
-            else {sub = last;}
+        for (char c : s){
+            current = symbols[c];
+            if (last == -1){
+                last = current;
+            } 
+            else if (last < current) {
+                total += (current - last);
+                last = -1;
+            } else {
+                total += last;
+                last = current;
+            }
         }
 
-        return (s.length() == 1) ? symbols[s[0]] : total + (current - sub);
+        if (last > 0) total += last;
+
+        return total;
     }
 };
