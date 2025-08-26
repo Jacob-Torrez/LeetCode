25c7d8724bae606192bc0ccdf22668715522c688
Jacob Torrez <72826559+Jacob-Torrez@users.noreply.github.com>
2025-08-26 12:27:34 -0700

    Add 27_remove_element.cpp - LeetCode Problem 27

---
+++ 27_remove_element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if (nums.empty()) return 0;
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right){
+            if (nums[left] == val){
+                nums[left] = nums[right];
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return left;
+    }
+};
